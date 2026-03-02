972f137e2ad08d28325e443fb54a1e622fbb810f
Hyunseop Kim <gustjq2580@gmail.com>
2026-03-02 14:09:07 +0900

    [feature] 서버 상태 점검 기능 추가

---
+++ src/main/java/com/todaybread/server/system/HealthController.java
@@ -0,0 +1,18 @@
+package com.todaybread.server.system;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 서버 정보를 체크하기 위한 컨트롤러입니다.
+ */
+@RequestMapping("/api/system")
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String getServerStatus(){
+        return "UP";
+    }
+}
