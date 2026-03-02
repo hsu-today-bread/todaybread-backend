d97e0d87df82a30e2af6d35b86a325e65512c881
Hyunseop Kim <gustjq2580@gmail.com>
2026-03-02 19:57:09 +0900

    [refactor] CustomException 주석 추가 (#4)

--- src/main/java/com/todaybread/server/global/exception/CustomException.java
+++ src/main/java/com/todaybread/server/global/exception/CustomException.java
@@ -2,6 +2,10 @@ package com.todaybread.server.global.exception;
 
 import lombok.Getter;
 
+/**
+ * 특정 오류 및 예외 처리 용 사용자 정의 예외입니다.
+ * 기존 예외 처리 및 오류 발생 시, 에러 코드를 삽입하고 파싱합니다.
+ */
 @Getter
 public class CustomException extends RuntimeException {
     private final ErrorCode errorCode;
