ce21278e807b9d69f201552bd1c469b17351e3c4
Hyunseop Kim <gustjq2580@gmail.com>
2026-03-06 19:52:12 +0900

    Revert "Feature/db setup (#9)"
    This reverts commit 61b333d604dde07e8623d9a46246581f03e13c0e.

--- src/main/java/com/todaybread/server/domain/user/dto/UserRegisterRequest.java
+++ src/main/java/com/todaybread/server/domain/user/dto/UserRegisterRequest.java
@@ -7,15 +7,13 @@ import jakarta.validation.constraints.NotBlank;
  * 회원가입 요청 DTO
  *
  * @param email 이메일
- * @param name 실명
- * @param nickname 닉네임
+ * @param nickName 닉네임
  * @param password 패스워드
  * @param phoneNumber 전화번호
  */
 public record UserRegisterRequest(
         @NotBlank @Email String email,
-        @NotBlank String name,
-        @NotBlank String nickname,
+        @NotBlank String nickName,
         @NotBlank String password,
-        String phoneNumber) {
+        @NotBlank String phoneNumber) {
 }
