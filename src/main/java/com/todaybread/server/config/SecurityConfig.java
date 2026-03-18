067ca314b37a345d011fd56baa06a927f81c3218
Hyunseop Kim <gustjq2580@gmail.com>
2026-03-18 12:31:16 +0900

    [fix] JWT 인증 중복체크 부분 예외 처리 완료

--- src/main/java/com/todaybread/server/config/SecurityConfig.java
+++ src/main/java/com/todaybread/server/config/SecurityConfig.java
@@ -69,6 +69,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/user/register",
                                 "/api/user/login",
+                                "/api/user/exist/**",
                                 "/api/auth/reissue",
                                 "/api/system/health",
                                 "/swagger-ui/**",
